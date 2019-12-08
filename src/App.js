9ee2987c0f752b6100d50bd27e55cab0c48ffb4d
Diego Almirón <diegoalmesp@gmail.com>
2019-12-08 19:23:56 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
