ea0e45d8e641355657a03d4b58db91f4b048d4c3
Diego Almirón <diegoalmesp@gmail.com>
2019-12-08 21:53:20 -0300

    nesting components + sass

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size ? size : ""} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className="background-image"
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+MenuItem.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default MenuItem;
