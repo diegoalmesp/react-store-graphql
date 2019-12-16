7ebb1c9948b9ace6d7d2f0f49a7918b24d6cad0c
Diego Almirón <diegoalmesp@gmail.com>
2019-12-15 23:08:32 -0300

    added styles for google buttons

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
-  <button className="custom-button" {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
     {children}
   </button>
 );
 
 CustomButton.propTypes = {
+  isGoogleSignIn: PropTypes.bool,
   children: PropTypes.node.isRequired,
 };
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,8 +48,12 @@ function SignIn() {
           required
         />
 
-        <CustomButton type="submit">Sign In</CustomButton>
-        <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
+        <div className="buttons">
+          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign In With Google
+          </CustomButton>
+        </div>
       </form>
     </div>
   );
